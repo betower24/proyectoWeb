692dc83b40bc055b5225de423349e2c17396e5a2
betower24 <127764841+betower24@users.noreply.github.com>
2025-11-21 14:51:58 -0600

    restore api/wsgi.py + app = application (fix definitivo Windows)

---
+++ api/wsgi.py
@@ -0,0 +1,8 @@
+﻿import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoWeb.settings')
+application = get_wsgi_application()
+
+# ¡OBLIGATORIO PARA VERCEL!
+app = application
