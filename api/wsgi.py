d7198fe7e42482c57e597562242cc1c0e24d401c
betower24 <127764841+betower24@users.noreply.github.com>
2025-11-21 13:54:15 -0600

    fix: ruta correcta api/wsgi.py + app = application (último fix Vercel)

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,8 @@
-# api/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoWeb.settings')
 application = get_wsgi_application()
+
+# ESTA LÍNEA ES LA QUE VER CEL BUSCA → ¡¡OBLIGATORIA!!
+app = application
